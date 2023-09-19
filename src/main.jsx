a75364fcddb8ffc5f71e78f5f5e0fbc581084a21
Shahid Ali <beingshahidali@gmail.com>
2023-09-19 14:15:06 +0530

    Detailed page added and css

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import Home from "../pages/Home"
 import About from "../pages/About"
 import "./server"
 import Vans from "../pages/Vans"
+import VanDetail from '../pages/vanDetail';
+
 
 function Main() {
   return (
@@ -28,6 +30,7 @@ function Main() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail/>} />
       </Routes>
       {/* ________________________________________________________________________________________________________ */}
 
