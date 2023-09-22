80e67e4a777f95dd471640350d5425214fd16a36
Shahid Ali <beingshahidali@gmail.com>
2023-09-22 15:39:33 +0530

    added files in host (info,price and photos)

--- src/main.jsx
+++ src/main.jsx
@@ -3,57 +3,41 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "../pages/Home"
 import About from "../pages/About"
-import "./server"
 import Vans from "../pages/Vans/Vans"
-import VanDetail from '../pages/Vans/VanDetail';
-import Layout from '../component/Layout'
-import Dashboard from '../pages/Host/Dashboard';
-import Reviews from '../pages/Host/Reviews';
-import Income from '../pages/Host/Income';
-import HostLayout from '../component/HostLayout';
-import HostVans from '../pages/Host/HostVans';
-import HostVanDetail from '../pages/Host/HostVanDetail';
+import VanDetail from "../pages/Vans/VanDetail"
+import Dashboard from "../pages/Host/Dashboard"
+import Income from "../pages/Host/Income"
+import Reviews from "../pages/Host/Reviews"
+import HostVans from "../pages/Host/HostVans"
+import HostVanDetail from "../pages/Host/HostVanDetail"
+import Layout from "../components/Layout"
+import HostLayout from "../components/HostLayout"
+
+import "./server"
 
 function Main() {
   return (
     <BrowserRouter>
-
-   { /* ________________________________________________Routes___________________________________________________ */}
-     <Routes>
-        <Route element={<Layout/>}>
-
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            
-            <Route path="vans">
-                <Route index element={<Vans />} />
-                <Route path=":id" element={<VanDetail />} />
-            </Route>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
           
-
-            <Route path="host" element={<HostLayout />}>
-
-                <Route index element={<Dashboard />} /> 
-                <Route path="income" element={<Income />} />
-                <Route path="reviews" element={<Reviews />} />
-                <Route path="vans" element={<HostVans />} />
-                <Route path="vans/:id" element={<HostVanDetail />} />
-
-
-                
-            </Route>
-
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />} />
+          </Route>
         </Route>
       </Routes>
-      {/* ________________________________________________________________________________________________________ */}
-
-
-    </BrowserRouter> 
-    
+    </BrowserRouter>
   )
 }
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(<Main/>);
-
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<Main />);
