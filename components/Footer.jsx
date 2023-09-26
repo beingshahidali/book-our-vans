003151ed13f594926965e745ec371c99eeebb67c
Shahid Ali <beingshahidali@gmail.com>
2023-09-26 19:32:29 +0530

    files changed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,6 @@ import React from "react"
 
 export default function Footer() {
     return (
-        <>
-        <footer>&#169;𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗱 𝗯𝘆 𝗦𝗵𝗮𝗵𝗶𝗱 𝗔𝗹𝗶</footer>
-        </>
+        <footer>&#169; 2022 #VANLIFE</footer>
     )
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,55 +1,51 @@
 import React from "react"
 import { Link, NavLink } from "react-router-dom"
-import userURL from "..\\assets\\images\\user.png"
+import imageUrl from "/assets/images/avatar-icon.png"
+
 export default function Header() {
-    /**
-     * Challenge - part 1:
-     * Make the main navbar indicate the currently-active route. (You can
-     * leave the home link alone, since it's doubling as our logo. Only
-     * make changes to the /host, /about, and /vans links)
-     * 
-     * Use the following CSS rules:
-     *      font-weight: bold;
-     *      text-decoration: underline;
-     *      color: #161616;
-     * 
-     * You can use either inline styles or a className.
-     */
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+
+    function fakeLogOut() {
+        localStorage.removeItem("loggedin")
+    }
+
     return (
         <header>
-         <Link className="site-logo" to="/">
+             <Link className="site-logo" to="/">
             <span className="logo-text" style={{ color: '#354B5E', textShadow: '.4px .4px .4px #1F2D3A' }}>Book</span>
             <span className="logo-text" style={{ color: '#E3A008', textShadow: '.4px .4px .4px #B27D04' }}>Our</span>
             <span className="logo-text" style={{ color: '#BF0603', textShadow: '.4px .4px .4px #780202' }}>Vans</span>
-        </Link>
-
-
-
+           </Link>
             <nav>
-                <NavLink 
+                <NavLink
                     to="/host"
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Host
                 </NavLink>
-                <NavLink 
+                <NavLink
                     to="/about"
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     About
                 </NavLink>
-                <NavLink 
+                <NavLink
                     to="/vans"
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Vans
                 </NavLink>
                 <Link to="login" className="login-link">
-                    <img 
-                        src={userURL} 
+                    <img
+                        src={imageUrl}
                         className="login-icon"
                     />
                 </Link>
+                <button onClick={fakeLogOut}>X</button>
             </nav>
         </header>
     )
